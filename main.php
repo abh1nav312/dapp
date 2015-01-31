0ba12309550dd4a1a5e9aaca37c8ffeaedc1ee14
Abhinav Pandey <abh1nav@outlook.com>
2015-01-31 14:48:33 +0530

    First frontend commit
    Basic Layout

--- main.php
+++ main.php
@@ -1,2 +1,52 @@
-<?php
-    echo 'Hello world!';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="Content/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+        <link href="Content/css/bootstrap.css" type="text/css" rel="stylesheet" />
+        <link href="Content/css/layout.css" type="text/css" rel="stylesheet" />
+        <link href="font-awesome-4.2.0/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+        <script src="Content/js/jquery-2.1.1.js" type="text/javascript"></script>
+        <script src="Content/js/layout.js" type="text/javascript"></script>
+        <script src="Content/js/bootstrap.js" type="text/javascript"></script>
+        <title>Movies4U</title>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <nav>
+                <a href="#" class="brand span6" style="text-align:center;font-size:x-large;color:black;">Movies4U</a>
+                <ul class="nav" style="margin-right:10%">
+                    <li class="span4"><form class="navbar-search"accept-charset="utf-8">
+                <input type="text" placeholder="Search">
+                </form></li>
+                    
+                    <li><button class="btn btn-primary">LogIn with Facebook</button>
+                    </ul>
+
+            </nav>
+        </nav>
+    </head>
+    <body>
+      <div></div>
+    </body>
+    <footer id="footer" class="footer">
+        <nav class="navbar navbar-fixed-bottom">
+            <nav>
+            <ul class="nav" style="margin-left:13%">
+                <li><a href="#">About</a></li>
+                    <li><a href="#" >Help</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#" >Contact</a></li>
+                <li><a href="#">Advertise</a></li>
+            </ul>
+            <ul class="nav" style="float:right;margin-right:13%">
+                <li><a href="#" >Terms</a></li>
+                <li><a href="#" >Privacy</a></li>
+                <li><a href="#" ><i class="fa fa-copyright">Copyright 2015</i></a></li>
+                <li><a href="#">Made in India by DAPP</a></li>
+            </ul>
+            </nav>
+        </nav>
+    </footer> 
+</html>
