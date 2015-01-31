16de935d4baafd5752285bf278255d0ca741a935
dheerajdac <dheerajdac@gmail.com>
2015-01-31 07:21:50 +0000

    Update user.php

--- user.php
+++ user.php
@@ -1,28 +1,118 @@
+ Skip to content
+This repository
+
+    Explore
+    Gist
+    Blog
+    Help
+
+    dheerajdac dheerajdac
+
+    748
+    3,569
+    3,298
+
+facebookarchive/facebook-php-sdk
+
+facebook-php-sdk/examples/example.php
+Fosco Marotto gfosco 18 days ago
+This SDK is deprecated. Use v4, see readme for link.
+
+6 contributors
+Naitik Shah Paul Tarjan Øyvind Sean Kinsey Stanislav Chistenko Jerry Cain Fosco Marotto
+108 lines (92 sloc) 2.957 kb
 <?php
-
-
-define('FACEBOOK_SDK_V4_SRC_DIR', '/facebook-php-sdk-v4-4.0-dev/src/Facebook/');
-require __DIR__ . '/facebook-php-sdk-v4-4.0-dev/autoload.php';
-
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-
-FacebookSession::setDefaultApplication('342504839278231','2a43602eaa0faef736081ea6ab0d30e6');
-
-
-$helper = new FacebookRedirectLoginHelper('google.com');
-$loginUrl = $helper->getLoginUrl();
-echo <<<_END
-	<html>
-	<head>
-	<title>login_page</title>
-	</head>
-	<body>
-		
-		<a href=$loginUrl>sing up?</a>
-	</body>
-	</html>
-_END;
+/**
+This SDK is deprecated. Please use the new SDK found here: https://github.com/facebook/facebook-php-sdk-v4
+*/
+/**
+* Copyright 2011 Facebook, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may obtain
+* a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+require '../src/facebook.php';
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+'appId' => '344617158898614',
+'secret' => '6dc8ac871858b34798bc2488200e503d',
+));
+// Get User ID
+$user = $facebook->getUser();
+// We may or may not have this data based on whether the user is logged in.
+//
+// If we have a $user id here, it means we know the user is logged into
+// Facebook, but we don't know if the access token is valid. An access
+// token is invalid if the user logged out of Facebook.
+if ($user) {
+try {
+// Proceed knowing you have a logged in user who's authenticated.
+$user_profile = $facebook->api('/me');
+} catch (FacebookApiException $e) {
+error_log($e);
+$user = null;
+}
+}
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+$logoutUrl = $facebook->getLogoutUrl();
+} else {
+$loginUrl = $facebook->getLoginUrl();
+}
+// This call will always work since we are fetching public data.
+$naitik = $facebook->api('/naitik');
 ?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+<title>php-sdk</title>
+<style>
+body {
+font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+}
+h1 a {
+text-decoration: none;
+color: #3b5998;
+}
+h1 a:hover {
+text-decoration: underline;
+}
+</style>
+</head>
+<body>
+<h1>php-sdk</h1>
+<?php if ($user): ?>
+<a href="<?php echo $logoutUrl; ?>">Logout</a>
+<?php else: ?>
+<div>
+Login using OAuth 2.0 handled by the PHP SDK:
+<a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+</div>
+<?php endif ?>
+<h3>PHP Session</h3>
+<pre><?php print_r($_SESSION); ?></pre>
+<?php if ($user): ?>
+<h3>You</h3>
+<img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+<h3>Your User Object (/me)</h3>
+<pre><?php print_r($user_profile); ?></pre>
+<?php else: ?>
+<strong><em>You are not Connected.</em></strong>
+<?php endif ?>
+<h3>Public profile of Naitik</h3>
+<img src="https://graph.facebook.com/naitik/picture">
+<?php echo $naitik['name']; ?>
+</body>
+</html>
+
+
+
