c82483d9f4ba0800b11aef6d00ab67c15c651c3c
Abhinav Pandey <abh1nav@outlook.com>
2015-01-31 12:44:16 +0530

    PHP SDK
    PHP SDK for FB

---
+++ Content/js/layout.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+	$("#footer").css({'background-color': 'teal',
+    'position': 'fixed',
+    'bottom':'0',
+    'margin-left':'0',
+    'clear':'both',
+    'width':'100%'});
+	
+	
+	
+	
+
+});
